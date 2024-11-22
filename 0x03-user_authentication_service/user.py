d48f7c30ec1f81d7d1113593ad5e00bc51a34805
Saviour Assandoh <asandohs@gmail.com>
2024-11-22 02:38:42 +0000

    task 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -6,7 +6,9 @@ User Authentication
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class User(Base):
     """Create table for Users
