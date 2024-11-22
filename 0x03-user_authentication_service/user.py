a824e45d2b69ad8956c7811666fbcd7a49b3f96c
Saviour Assandoh <asandohs@gmail.com>
2024-11-22 01:42:12 +0000

    task 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,8 +15,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String(300), nullable=False)
-    hashed_password = Column(String(300), nullable=False)
-    session_id = Column(String(300))
-    reset_token = Column(String(300))
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
 
